24dcf86267681bab4cb1ec452e3c7f97adf1b894
veniwan <veniwan@163.com>
2019-01-29 10:13:34 +0800

    Update detect_evil_ip.sh

--- detect_evil_ip.sh
+++ detect_evil_ip.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # auto add evil detect ip by ipset
-# 简单探测：以php为特征，单日超过150次请求加入黑名单
+# 简单探测：以php为特征，单日超过200次请求加入黑名单
 # */10 * * * * sh /data/code/detect_evil_ip.sh &> /dev/null
 # 日志轮询
 # 1 0 * * * sh /data/code/detect_evil_ip.sh rotate &> /dev/null
@@ -9,22 +9,28 @@
 # /usr/sbin/service iptables restart
 
 NOWDATE=`/usr/bin/date +%F`
-LOGPATH=/data/xiaowanzi/logs/uwsgi.log
+LOGPATH1=/data/xiaowanzi/logs/uwsgi.log
+LOGPATH2=/data/nginx_logs/access.log
+LOGPATH3=/data/nginx_logs/error.log
+LOGPATH4=/data/nginx_logs/xiaowanzi.log
 
-IPS=`/usr/bin/cat $LOGPATH | /bin/grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' | /bin/sort | /usr/bin/uniq  -c | /usr/bin/awk '$1>150{print $2}'`
+IPS=`/usr/bin/cat $LOGPATH{1..4} | /bin/grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' | /bin/sort | /usr/bin/uniq  -c | /usr/bin/awk '$1>200{print $2}'`
 
 /bin/grep -q blacklist /etc/sysconfig/iptables || \
 /usr/sbin/iptables -A INPUT -p tcp -m set --match-set blacklist src -j DROP
 /usr/sbin/ipset create blacklist hash:ip
 for ip in $IPS
 do
-   /bin/grep "$ip" $LOGPATH | /bin/grep -q 'php' && \
+   /bin/grep "$ip" $LOGPATH{1..4} | /bin/grep -q 'php' && \
        /usr/sbin/ipset add blacklist $ip
 done
 /usr/sbin/ipset save > /etc/sysconfig/ipset
 /usr/sbin/service iptables save
 
 if [ "$1" = "rotate" ]; then
-    /bin/cp -f $LOGPATH{,.$NOWDATE}
-    > $LOGPATH
+    for LOGPATH in $LOGPATH{1..4}
+    do
+        /bin/cp -f $LOGPATH{,.$NOWDATE}
+        > $LOGPATH
+    done
 fi
